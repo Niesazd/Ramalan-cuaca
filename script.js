b8e65bff1c66cbe263c2849e1424b20e58431968
Niesazd <khairunnisadzeidee@gmail.com>
2024-01-31 11:49:14 +0800

    Added function for city search in script.js

--- script.js
+++ script.js
@@ -10,8 +10,6 @@ function mainDisplay(response) {
   let wind = document.querySelector(".wind-data");
 
   city.innerHTML = response.data.city;
-  //    let countryData = response.data.country;
-  //   country.innerHTML = countryData.toUpperCase()
   country.innerHTML = response.data.country.toUpperCase();
   currentTemp.innerHTML = Math.round(response.data.temperature.current) + "°C";
   currentTempDesc.innerHTML = response.data.condition.description;
@@ -26,13 +24,33 @@ console.log(apiUrl);
 
 axios.get(apiUrl).then(mainDisplay);
 
+function cityTemperatureFunction(response) {
+  let city = document.querySelector(".city-data");
+  let country = document.querySelector(".country-data");
+  let emoji = document.querySelector(".current-temperature-emoji");
+  let currentTemp = document.querySelector(".current-temperature-data");
+  let currentTempDesc = document.querySelector(
+    ".current-temperature-description"
+  );
+  let humidity = document.querySelector(".humidity-data");
+  let wind = document.querySelector(".wind-data");
+
+  city.innerHTML = response.data.city;
+  country.innerHTML = response.data.country.toUpperCase();
+  currentTemp.innerHTML = Math.round(response.data.temperature.current) + "°C";
+  currentTempDesc.innerHTML = response.data.condition.description;
+  humidity.innerHTML = `${response.data.temperature.humidity}%`;
+  wind.innerHTML = `${response.data.wind.speed} km/h`;
+}
+
 function searchCityFunction(response) {
   response.preventDefault();
   let searchInputElement = document.querySelector(".search-city");
   let city = searchInputElement.value;
 
-  let h1 = document.querySelector(".city-data");
-  h1.innerHTML = city;
+  let apiKey = "5fe7120d92tc7592472edba320co9e7b";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(cityTemperatureFunction);
 }
 
 let searchCity = document.querySelector(".input-search");
